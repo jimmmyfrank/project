d75cd62e8a4addf3d758765de0b6a857f353abdf
Jimmy frank <tjimmyfrank@gmail.com>
2026-05-27 14:16:35 +0200

    added backend functionalities

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,13 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
+    server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  }
 })
